d770581c2f5e60284ca8dff8ac00a2a7f3ead0a4
Brandon Lucia <blucia@gmail.com>
2016-11-24 13:27:56 -0500

    more interesting lpf demo

--- demo_lpfmod.sh
+++ demo_lpfmod.sh
@@ -1,1 +1,1 @@
- ./lpf.pl -cutoff 300 -cutoffmod <(./wave.pl -wave tri -freq 3) -resonance 1.8 -in <(./mix.pl -in1 <( ./wave.pl -wave sin -freq 640 -amp 1.0) -in2 <(./wave.pl -wave tri -freq 600 -amp 1.0) )  | ./play.sh
+ ./lpf.pl -cutoff 400 -cutoffmod <(./wave.pl -wave tri -freq 10 -freqmod <(./wave.pl -wave sin -freq 2) ) -resonance 2.0 -in <(./mix.pl -in1 <( ./wave.pl -wave sin -freq 610 -amp 1.0 -freqmod <(./wave.pl -wave tri -freq 100) ) -in2 <(./wave.pl -wave tri -freq 600 -amp 1.0) )  | ./play.sh

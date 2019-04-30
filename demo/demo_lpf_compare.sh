e1497a8ceb2854341355ce4b079b4af2029c328e
Brandon Lucia <blucia@gmail.com>
2019-04-29 21:04:18 -0400

    added demo of lpf

--- demo/demo_lpf_compare.sh
+++ demo/demo_lpf_compare.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-lpfSimp.pl -in \
-  <(mix.pl \
+mix.pl \
     -in1 <(mix.pl \
               -in1 <(wave.pl -wave sin -freq 1260  -amp 0.5) \
               -in2 <(wave.pl -wave sin -freq 1100 -amp 0.5))  \
     -in2 <(mix.pl \
               -in1 <(wave.pl -wave sin -freq 120 -amp 0.5) \
               -in2 <(wave.pl -wave sin -freq 100 -amp 0.5))  \
-  ) \
 | play.sh
 

24bbf16ccf1c18d66cf402e42bffa3748e9426f5
Brandon Lucia <blucia@gmail.com>
2019-04-29 23:24:20 -0400

    added plot-symmetric play backend

---
+++ synth.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Getopt::Long;
+
+my $SampRate = 44100;
+my $SampSize = 2;
+my $IN1File = "";
+my $amp = 1.0;
+my $numsamps = 0;
+
+GetOptions ("srate=i"  => \$SampRate,
+            "ssize=i"  => \$SampSize,
+            "amp=f"    => \$amp,
+            "numsamps=n"    => \$numsamps,
+            "in=s"     => \$IN1File)
+or die("Error in command line arguments\n");
+
+my $IN1;
+open($IN1,'<', $IN1File);
+binmode($IN1,":raw") || die "cannot binmode IN1";
+
+
+binmode(STDOUT,":raw") || die "cannot binmode STDOUT";
+
+if($numsamps == 0){
+
+  while(1){
+  
+    my $samp1raw;
+    read($IN1, $samp1raw, $SampSize); #TODO
+    my $samp1 = unpack "S", $samp1raw;
+  
+    print pack 'S', ($samp1); 
+  
+  }
+
+}else{
+
+  my $i = 0;
+  while($i < $numsamps){
+  
+    my $samp1raw;
+    read($IN1, $samp1raw, $SampSize); #TODO
+    my $samp1 = unpack "S", $samp1raw;
+  
+    print pack 'S', ($samp1); 
+
+    $i++;
+  
+  }
+
+}

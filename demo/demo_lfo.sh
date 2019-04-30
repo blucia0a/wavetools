e2867ad8ea63543081cb190e4639d76811132609
Brandon Lucia <blucia@gmail.com>
2019-04-30 15:56:34 -0400

    added quiet to play and added better lfo demo

--- demo/demo_lfo.sh
+++ demo/demo_lfo.sh
@@ -1,14 +1,56 @@
 #!/bin/bash
 
+basspad_p=/tmp/basspad
+mkfifo $basspad_p;
+
+highpad_p=/tmp/highpad
+mkfifo $highpad_p;
+
 lpf.pl -in \
   <(mix.pl \
+ \
+ \
     -in1 <(mix.pl \
               -in1 <(wave.pl -wave sin  -freqmod <(wave.pl -wave sin  -freq 0.1) -freq 440 -amp 0.35) \
               -in2 <(wave.pl -wave sin  -ampmod <(wave.pl -wave sin  -freq 0.19) -freq 420 -amp 0.35))  \
+ \
+ \
+ \
     -in2 <(mix.pl \
-              -in1 <(wave.pl -wave sin  -freqmod <(wave.pl -wave sin  -freq 0.1 -freqmod <(wave.pl -wave tri -freq 0.05)) -freq 220 -amp 0.35) \
-              -in2 <(wave.pl -wave sin  -ampmod <(wave.pl -wave sin  -freq 0.23) -freq 210 -amp 0.35))  \
+              -in1 <(wave.pl -wave sin  \
+                             -freqmod <(wave.pl -wave sin  \
+                                                -freq 0.2 \
+                                                -freqmod <(wave.pl -wave tri \
+                                                                   -freq 0.05)\
+                                       ) \
+                             -freq 220 \
+                             -amp 0.35) \
+                                        \
+              -in2 <(wave.pl -wave sin  \
+                             -ampmod <(wave.pl -wave sin \
+                                               -freq 0.23) \
+                             -freq 210 \
+                             -amp 0.35)\
+         )  \
   ) \
-  -cutoff 400.0 -resonance 1.4 -cutoffmod <(wave.pl -wave tri -freq 0.15) \
-| play.sh
+ \
+ \
+ \
+  -cutoff 400.0 -resonance 1.4 -cutoffmod <(wave.pl -wave tri -freq 0.15) > /tmp/basspad &
+basspad=$!
 
+mix.pl -in1 <(wave.pl -wave sin -freq 440 -amp 0.4 -freqmoddepth 1.0 -freqmod <(wave.pl -wave sin -freq 0.01 -freqmod <(wave.pl -wave tri -freq 0.1 ) -freqmoddepth 1.0 )) \
+-in2 <(wave.pl -wave sin -freq 600 -amp 0.35 -freqmoddepth 1.0 -freqmod <(wave.pl -wave sin -freq 0.007 -freqmod <(wave.pl -wave tri -freq 0.05 ) -freqmoddepth 1.0 )) > /tmp/highpad &
+highpad=$!
+
+trap "echo \"Shutting down. basspad $basspad highpad $highpad\"; \
+      kill $basspad; \
+      kill $highpad; echo \"Removing pipes\"; \
+      rm /tmp/highpad; rm /tmp/basspad;"\
+      SIGINT SIGHUP SIGTERM
+
+echo "syncs" 
+echo "bass: $basspad"
+echo "high: $highpad"
+
+synth.pl -in <(mix.pl -in1 /tmp/basspad -in2 /tmp/highpad) | play.sh

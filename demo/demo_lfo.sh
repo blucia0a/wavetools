9a461afe51517ded3d86c97373805b102c16ee2b
Brandon Lucia <blucia@gmail.com>
2019-04-30 12:38:39 -0400

    added neat demo of lfos on lfos on lfos

--- demo/demo_lfo.sh
+++ demo/demo_lfo.sh
@@ -3,12 +3,12 @@
 lpf.pl -in \
   <(mix.pl \
     -in1 <(mix.pl \
-              -in1 <(wave.pl -wave sin  -freqmod <(wave.pl -wave sin  -freq 1) -freq 840 -amp 0.5) \
-              -in2 <(wave.pl -wave sin  -ampmod <(wave.pl -wave sin  -freq 10) -freq 830 -amp 0.5))  \
+              -in1 <(wave.pl -wave sin  -freqmod <(wave.pl -wave sin  -freq 0.1) -freq 440 -amp 0.35) \
+              -in2 <(wave.pl -wave sin  -ampmod <(wave.pl -wave sin  -freq 0.19) -freq 420 -amp 0.35))  \
     -in2 <(mix.pl \
-              -in1 <(wave.pl -wave sin  -freqmod <(wave.pl -wave sin  -freq 26) -freq 520 -amp 0.5) \
-              -in2 <(wave.pl -wave sin  -ampmod <(wave.pl -wave sin  -freq 70) -freq 500 -amp 0.5))  \
+              -in1 <(wave.pl -wave sin  -freqmod <(wave.pl -wave sin  -freq 0.1 -freqmod <(wave.pl -wave tri -freq 0.05)) -freq 220 -amp 0.35) \
+              -in2 <(wave.pl -wave sin  -ampmod <(wave.pl -wave sin  -freq 0.23) -freq 210 -amp 0.35))  \
   ) \
-  -cutoff 840.0 -resonance 1.2 \
+  -cutoff 400.0 -resonance 1.4 -cutoffmod <(wave.pl -wave tri -freq 0.15) \
 | play.sh
 

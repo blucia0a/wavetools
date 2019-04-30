58c15ea695dfce063eaba311d0c6504d00b3f3c2
Brandon Lucia <blucia@gmail.com>
2019-04-30 12:15:00 -0400

    random changes to the lpfmod demo

--- demo/demo_lpfmod.sh
+++ demo/demo_lpfmod.sh
@@ -1,1 +1,1 @@
- lpf.pl -cutoff 400 -cutoffmod <(wave.pl -wave tri -freq 10 -freqmod <(wave.pl -wave sin -freq 2) ) -resonance 2.0 -in <(mix.pl -in1 <( wave.pl -wave sin -freq 610 -amp 1.0 -freqmod <(wave.pl -wave tri -freq 100) ) -in2 <(wave.pl -wave tri -freq 600 -amp 1.0) )  | play.sh
+plot.pl -numsamps 10000 -in <( lpf.pl -cutoff 400 -cutoffmod <(wave.pl -wave tri -freq 100 -freqmod <(wave.pl -wave sin -freq 2) ) -resonance 2.0 -in <(mix.pl -in1 <( wave.pl -wave sin -freq 610 -amp 1.0 -freqmod <(wave.pl -wave tri -freq 100) ) -in2 <(wave.pl -wave tri -freq 600 -amp 1.0) ))  | plot.sh

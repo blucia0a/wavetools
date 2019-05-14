c43994227deb390f891307a19653335c8878d4ec
Brandon Lucia <blucia@gmail.com>
2019-05-14 10:51:49 -0400

    deleted old demo scripts

--- demo/demo_mod.sh
+++
@@ -1,6 +0,0 @@
-lpf.pl -cutoff 300 -resonance 1.8 -in \
-         <( wave.pl -wave tri -freq 640 -amp 1.0 \
-                      -freqmod <(wave.pl -wave tri -freq 30 \
-                                           -freqmod <(wave.pl -wave tri -freq 10) ) \
-                      -ampmod <(wave.pl -wave tri -freq 9) ) \
-| play.sh

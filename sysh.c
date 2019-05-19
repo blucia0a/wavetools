550f2f436f907c3ab98f8ec10a72bf670323c7b4
Brandon Lucia <blucia@gmail.com>
2019-05-19 17:53:12 -0400

    test lfo support

--- sysh.c
+++ sysh.c
@@ -11,49 +11,7 @@
 #include "wave.h"
 #include "wavloader.h"
 
-
-
-int main( int argc, char * argv[]){
-
-  /*Make standard out unbuffered for raw output*/
-  setbuf(stdout,NULL);
-
-  wavfile wav;
-  wavetable sine;
-  
-  wavloader_load(&wav, "wavlib/sine.wav");
-  memset(&sine,0,sizeof(wavetable));
-  wavtab_from_wavfile(&sine, &wav);
-  
-  wavfile meowav;
-  wavetable meowtab;
-
-  wavloader_load(&meowav, "wavlib/meow2.wav");
-  memset(&meowtab,0,sizeof(wavetable));
-  wavtab_from_wavfile(&meowtab, &meowav);
-
-
-  module *mo = out_new();
-
-  module *mw1 = wave_new( &sine, 220);
-  module *mw2 = wave_new( &sine, 440);
-  module *mw3 = wave_new( &meowtab, 2);
-  module *mw4 = wave_new( &meowtab, 40);
-
-  module *mx = mixn_new();
-  
-  module *mlpf = lpfsimp_new( 440., 1.4 );
-
-  mixn_addin(mx->mod,mw1);
-  mixn_addin(mx->mod,mw2);
-  mixn_addin(mx->mod,mw3);
-  mixn_addin(mx->mod,mw4);
-
-  lpfsimp_setin(mlpf->mod,mx);
-
-  out_setin(mo->mod,mlpf);
-
-  /*
+ /*Synth sketch
  *
  * w1 | w2 | w3 | w4 
  * \               /
@@ -66,16 +24,39 @@ int main( int argc, char * argv[]){
  *         o
  */
 
+int main( int argc, char * argv[]){
+
+  /*Make standard out unbuffered for raw output*/
+  setbuf(stdout,NULL);
+
+  wavetable *sine = wavetable_new("wavlib/sine.wav");
+  //wavetable *sine = wavetable_new("/home/blucia/cvsandbox/AKWF-FREE/AKWF/AKWF_epiano/AKWF_epiano_0042.wav");
+  wavetable *meowtab = wavetable_new("wavlib/meow2.wav");
+
+  module *mo = out_new();
+
+  module *mw1 = wave_new( sine, 0.2);
+  //module *mw2 = wave_new( sine, 215);
+  //module *mw3 = wave_new( meowtab, 20);
+  //module *mw4 = wave_new( meowtab, 19);
+
+  //module *mx = mixn_new();
+  
+  //module *mlpf = lpfsimp_new( 440., 1.8 );
+
+  //mixn_addin(mx->mod,mw1);
+  //mixn_addin(mx->mod,mw2);
+  //mixn_addin(mx->mod,mw3);
+  //mixn_addin(mx->mod,mw4);
+
+  //lpfsimp_setin(mlpf->mod,mx);
+
+  out_setin(mo->mod,mw1);
+
   while(1){
 
     mo->next((void*)mo->mod);
-/*    sample ns;
 
-    ns = mlpf1->next((void*)mlpf1->mod);
-    //ns = mx1->next((void*)mx1->mod);
-
-    fwrite(&ns,sizeof(sample),1,stdout);
-*/
   }
 
 }

3d864870b831226b308950fbc8046cfd86d8f1c0
Brandon Lucia <blucia@gmail.com>
2019-06-05 12:38:01 -0400

    added ncurses keyboard.  probably needs to run in a separate process...

---
+++ vkb.h
@@ -0,0 +1,51 @@
+#define k_a 97
+#define f_a 349.23 //F
+
+#define k_w 119
+#define f_w 369.99 //F#
+
+#define k_s 115
+#define f_s 391.99 //G
+
+#define k_e 101
+#define f_e 415.30 //G#
+
+#define k_d 100
+#define f_d 440. //A
+
+#define k_r 114
+#define f_r 466.16 //A#
+
+#define k_f 102
+#define f_f 493.88 //B
+
+#define k_g 103 
+#define f_g 523.25 //C
+
+#define k_y 121
+#define f_y 554.37 //C#
+
+#define k_h 104
+#define f_h 587.33 //D
+
+#define k_u 117
+#define f_u 622.25 //D#
+
+#define k_j 106
+#define f_j 659.26 //E
+
+float keyfreq[130] = {0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,0,0,0, 
+                   0,0,0,0,0,0,0,f_a,0,0, 
+                   f_d,f_e,f_f,f_g,f_h,0,f_j,0,0,0, 
+                   0,0,0,0,f_r,f_s,0,f_u,0,f_w, 
+                   0,f_y,0,0,0,0,0,0,0,0};
+
+
